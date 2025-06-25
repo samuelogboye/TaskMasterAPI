3adec669e993fc3d2a61bb7ee861bb824634408b
Samuel Ogboye <ogboyesam@gmail.com>
2025-06-26 00:12:50 +0100

    refactor: update configuration settings structure in config.py for clarity

--- app/config.py
+++ app/config.py
@@ -2,15 +2,16 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str 
+    DATABASE_URL: str
     PORT: int
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    model_config = {
+        "env_file": ".env",
+        "env_file_encoding": "utf-8"
+    }
 
 
 settings = Settings()
