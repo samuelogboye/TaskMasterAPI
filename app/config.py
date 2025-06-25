13489c4f832962e29bd02504fdfac5d4c9b5a69e
Samuel Ogboye <ogboyesam@gmail.com>
2025-06-25 22:25:21 +0100

    feat: update configuration settings to use environment variables for database and server settings

--- app/config.py
+++ app/config.py
@@ -2,8 +2,11 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./taskmaster.db"
-    PORT: int = 8000  # default value
+    DATABASE_URL: str 
+    PORT: int
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
